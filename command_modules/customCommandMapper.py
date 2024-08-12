7ccf16803da0ee3c52e35e318758b30266dad13e
RoshanNeelmegam <98408299+RoshanNeelmegam@users.noreply.github.com>
2024-08-12 10:46:53 -0400

    merging Nlevel changes to main

--- command_modules/customCommandMapper.py
+++ command_modules/customCommandMapper.py
@@ -5,9 +5,13 @@ from command_modules.lldpNeighbors import *
 from command_modules.showLogging import *
 
 mapCommand = {
-    'show ip interfaces brief': {'function': show_int_br, 'parent_command': 'show ip interfaces', 'is_static': True, 'dict_input': False},
+    'show ip interface brief': {'function': show_int_br, 'parent_command': 'show ip interface', 'is_static': True, 'dict_input': False},
     'show version': {'function': showVersion, 'parent_command': 'show version detail', 'is_static': True, 'dict_input': False},
-    'show interfaces status': {'function': show_interfaces_status, 'parent_command': 'show interfaces status', 'is_static': True, 'dict_input': False},
     'show lldp neighbors': {'function': lldpOutput, 'parent_command': 'show lldp neighbors detail', 'is_static': True, 'dict_input': True},
 }
 
+def handle_mapped_command(self, command):
+    map_info = mapCommand[command]
+    if map_info['is_static']:
+        input_data = self.command_searcher(map_info['parent_command']) if not map_info['dict_input'] else self.lldp_dictionary
+        return map_info['function'](input_data)
--- command_modules/routeLookup.py
+++ command_modules/routeLookup.py
@@ -1,18 +1,16 @@
 import re
 
 def parsing_routing_table(contents, routes):
+    # optimized this part for faster parsing
     # creating a routes dictionary: routes = {'default': ['0.0.0.0/0', '0.0.0.0/8', '32.55.116.0/26', '32.55.178.20/31', 'mgmt': ['0.0.0.0/8']}
-    vrfs_list_original = re.findall('VRF:.*', contents)
+    vrfs_list_original = re.findall(r'\nVRF: (\S+)', contents)
+    biglist = re.findall(r'(\nVRF: \S+|\d+\.\d+\.\d+\.\d+\/\d+)', contents)
     for vrf in vrfs_list_original:
-        # if vrf to work on is last in the list
-        if vrfs_list_original.index(vrf) == len(vrfs_list_original) - 1:
-            first_match = re.search(vrf, contents).span()[1]
-            second_match = first_match + re.search(r'------------- (show|bash).*', contents[first_match+1:]).span()[0]
-        else: 
-            first_match = re.search(vrf, contents).span()[1]
-            second_match = re.search('VRF: .*', contents[first_match+1:]).span()[0]
-            second_match += first_match - 1
-        routes[vrf.strip('VRF: ')] = re.findall(r'(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\/\d{1,2}', contents[first_match:second_match])
+        if vrf == 'Table_Ends_Here':
+            break
+        vrf_to_start_search = biglist.index(f'\nVRF: {vrf}') + 1
+        vrf_to_end_search = biglist.index(f'\nVRF: {vrfs_list_original[vrfs_list_original.index(vrf) + 1]}')
+        routes[vrf] = biglist[vrf_to_start_search:vrf_to_end_search]
 
 
 def creating_vrf_routing_table(routes):
@@ -57,27 +55,22 @@ def lookup(table, ip_address, vrf, matched_routes):
 
 
 def vrf_route_lookup(contents, address_match, vrf):
-    content = ''
-    pfirst_match = re.search(f'VRF: {vrf}', contents).span()[1]
-    pattern = re.compile("VRF:.*|------------- show.*")
-    psecond_match = pattern.search(contents[pfirst_match+1:]).span()[0]
+    lookup = '' 
+    pfirst_match = re.search(fr'VRF: {vrf}', contents).span()[1] # matches the first line of the vrf routing entry
+    pattern = re.compile("VRF:.*|------------- show.*") # pattern to know if the next show command start or if the next vrf in the routing entry starts
+    psecond_match = pattern.search(contents[pfirst_match+1:]).span()[0] # match for the last line in the routing for that vrf
     psecond_match += pfirst_match - 1
-    content = f' VRF: {vrf}' + '\n'
-    route_match = re.search(rf'[^vV][^iI][^aA][\s]+{address_match}.*(via|is directly connected).+', contents[pfirst_match:psecond_match])
-    content += route_match.group()
-    lnum = route_match.span()[1]
-    for line in contents[pfirst_match+lnum+1:(pfirst_match+lnum+1800)].splitlines():
-        via_match_line = re.search('   via [0-9].+', line)
-        if via_match_line:
-            content += '\n' + ("%-28s%-37s" % (25*" ",via_match_line.group()))
-        else:
-            break
-    return content
+    lookup = f'VRF: {vrf}' + '\n'
+    match = re.findall(rf'( [A-Z]+.*{address_match}.*)(\n\s+via.*|\n\s+directly.*)?(\n\s+via.*|\n\s+directly.*)?(\n\s+via.*|\n\s+directly.*)?(\n\s+via.*|\n\s+directly.*)?(\n\s+via.*|\n\s+directly.*)?(\n\s+via.*|\n\s+directly.*)?(\n\s+via.*|\n\s+directly.*)?(\n\s+via.*|\n\s+directly.*)?(\n\s+via.*|\n\s+directly.*)?(\n\s+via.*|\n\s+directly.*)?', contents[pfirst_match:psecond_match])
+    if match:
+        match = match[0]
+        lookup += ''.join(match)
+    return lookup
                 
                 
 def routing_table_ouput(contents, vrf):
     try:
-        first_match = re.search(f'VRF: {vrf}[\s].+', contents)
+        first_match = re.search(fr'VRF: {vrf}[\s].+', contents)
         first_line_no = first_match.span()[1]
         vrf_name = first_match.group()
         second_line_no = re.search("VRF:.*|------------- show.*", contents[first_line_no+1:]).span()[0]
@@ -85,3 +78,90 @@ def routing_table_ouput(contents, vrf):
         return f'{vrf_name} + \n + {contents[first_line_no:second_line_no]}'
     except AttributeError as e:
          return 'vrf does not exist'
+    
+def ip_host_route(contents, command):
+    try:
+        contents += '\n------------- EndsHere -------------'
+        try:
+            vrf = re.findall(r'.+vrf\s*(.*)\shost', command, re.IGNORECASE)[0]
+        except IndexError as e:
+            vrf = 'default'
+        start_output = re.search(rf'VRF: {vrf}', contents).span()[1]
+        end_output = re.search(rf'VRF:.*|------------- EndsHere -------------', contents[start_output:]).span()[0]
+        end_output += start_output
+        actual_output = f'\nshow ip route vrf {vrf} host\n'
+        actual_output += contents[start_output:end_output]
+        return(actual_output)
+    except Exception as e:
+        print('vrf does not exists')
+
+def ip_summary_route(contents, command):
+    try: 
+        contents += '\n------------- EndsHere -------------'
+        try:
+            vrf = re.findall(r'.+vrf\s*(.*)\ssummary', command, re.IGNORECASE)[0]
+        except IndexError as e:
+            vrf = 'default'
+        actual_output = f'\nshow ip route vrf {vrf} summary\n\n'
+        actual_output += '\n'.join(re.findall(r'Operating.*|Configured.*', contents, re.I))
+        start_output = re.search(rf'VRF: {vrf}', contents).span()[1]
+        end_output = re.search(rf'VRF:.*|------------- EndsHere -------------', contents[start_output:]).span()[0]
+        end_output += start_output
+        actual_output += '\n' + contents[start_output:end_output]
+        return(actual_output)
+    except Exception as e:
+        print('vrf does not exists')
+
+def handle_ip_route(self, command, match):
+    if command in ['show ip route kernel unprogrammed', 'show ip route vrf all detail', 'show ip route vrf all host', 'show ip route vrf all summary']:
+        return(self.command_searcher(command))
+    elif command in ['show ip route vrf all']:
+        return(self.command_searcher(command+" detail"))
+    else:
+        clist = command.split(' ')
+        clist_len = len(clist)
+        if clist_len == 3:
+                return(routing_table_ouput(self.routing_contents, 'default'))
+        elif clist_len == 4:
+                if clist[3] == 'host':
+                     return(ip_host_route(self.command_searcher('show ip route vrf all host'), command))
+                elif clist[3] == 'summary':
+                     return(ip_summary_route(self.command_searcher('show ip route vrf all summary'), command))
+                vrfc = 'default'
+                ip_add = clist[-1]
+        elif clist_len == 5:
+                if clist[3] == 'vrf':
+                    vrfc = clist[-1]
+                    return(routing_table_ouput(self.routing_contents, vrfc))
+                else:
+                    return('invalid input')
+        elif clist_len == 6:
+                if clist[5] == 'host':
+                     return(ip_host_route(self.command_searcher('show ip route vrf all host'), command))
+                elif clist[5] == 'summary':
+                     return(ip_summary_route(self.command_searcher('show ip route vrf all summary'), command))
+                try:
+                    vrfc = clist[-2]
+                    ip_add = clist[-1]
+                    if (vrfc not in self.routes) and (vrfc != 'all'):
+                        return('vrf does not exist')
+                except KeyError as e:
+                        return('invalid input')
+        else:
+                return('invalid input')
+        if (len(clist) == 6 or len(clist) == 4):
+            if vrfc == 'all':
+                 output = '\n'
+                 for vrf in self.routes:
+                      route_ip = lookup(self.vrf_routing_table, ip_add, vrf, self.matched_routes)
+                      self.matched_routes = []
+                      output += vrf_route_lookup(self.routing_contents, route_ip, vrf) + '\n\n' 
+                 return output
+            else: 
+                route_ip = lookup(self.vrf_routing_table, ip_add, vrfc, self.matched_routes)
+                if route_ip == 'Invalid ip address':
+                    return 'Invalid ip address'
+                else:
+                    self.matched_routes = []
+                    return vrf_route_lookup(self.routing_contents, route_ip, vrfc)
+                    
